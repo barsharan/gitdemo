8a3dbab7507a4d4490a508f737ed5ca53b189703
barsharan <barsharani597@gmail.com>
2023-10-26 20:54:14 +0530

    Add new invisible one list

---
+++ dom4.js
@@ -0,0 +1,7 @@
+// Get a reference to the 2nd item (index 1) and change its background color to green
+var secondItem = document.getElementById('items').getElementsByTagName('li')[1];
+secondItem.style.backgroundColor = 'green';
+
+// Get a reference to the 3rd item (index 2) and make it invisible
+var thirdItem = document.getElementById('items').getElementsByTagName('li')[2];
+thirdItem.style.display = 'none'; // This will hide the item

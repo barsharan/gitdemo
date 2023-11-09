cbbc2e87d741d2989fa011474ba56dddadbcd5aa
barsharan <barsharani597@gmail.com>
2023-11-09 19:04:27 +0530

    QuerySelector && QuerySelectorALL

---
+++ selector.js
@@ -0,0 +1,5 @@
+// Change the background color of the 2nd item to green
+document.getElementById("items").getElementsByTagName("li")[1].style.backgroundColor = "green";
+
+// Make the 3rd item invisible
+document.getElementById("items").getElementsByTagName("li")[2].style.display = "none";

1d886b807cb0e2ceeb1dea53f93abb4f2ed8deed
barsharan <barsharani597@gmail.com>
2023-12-09 19:19:08 +0530

    add deletecrud js file

---
+++ deletecruds.js
@@ -0,0 +1,107 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const myForm = document.getElementById("myForm");
+    const nameInput = document.getElementById("nameInput");
+    const emailInput = document.getElementById("emailInput");
+    const userDataElement = document.getElementById("userData");
+
+    // Initialize existing user data from local storage
+    let existingUserData = JSON.parse(localStorage.getItem("userData")) || [];
+
+    // Ensure existingUserData is always an array
+    if (!Array.isArray(existingUserData)) {
+        existingUserData = [];
+    }
+
+    // Function to display user data with delete icons
+    function displayUserData() {
+        userDataElement.innerHTML = "";
+        existingUserData.forEach((user, index) => {
+            const userHtml = `
+                <div>
+                    <p>User ${index + 1}: Name - ${user.name}, Email - ${user.email}</p>
+                    <span class="delete-icon" onclick="deleteUser('${user._id}')">&#10006;</span>
+                </div>`;
+            userDataElement.innerHTML += userHtml;
+        });
+    }
+
+    // Function to fetch user data from crudcrud.com
+    function fetchUserData() {
+        axios.get('https://crudcrud.com/api/59fd66bbab264efbbbcd1508a0684487/dashboard')
+            .then(response => {
+                const retrievedData = response.data || [];
+                console.log('Retrieved User Data:', retrievedData);
+
+                existingUserData = retrievedData;
+                localStorage.setItem("userData", JSON.stringify(existingUserData));
+                displayUserData();
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
+    }
+
+    // Display existing user data when the page loads
+    displayUserData();
+
+    // Fetch user data from crudcrud.com when the page loads
+    fetchUserData();
+
+    myForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const name = nameInput.value;
+        const email = emailInput.value;
+
+        if (name && email) {
+            // Create a user object
+            const user = { name, email };
+
+            // Add the user to the existing data
+            existingUserData.push(user);
+
+            // Update the local storage with the new user data
+            localStorage.setItem("userData", JSON.stringify(existingUserData));
+
+            // Clear the input fields
+            nameInput.value = "";
+            emailInput.value = "";
+
+            // Display the updated user data
+            displayUserData();
+
+            // Log the added user data to the console
+            console.log("Added User Data:", user);
+
+            // Make a POST request to crudcrud.com using Axios
+            axios.post('https://crudcrud.com/api/59fd66bbab264efbbbcd1508a0684487/dashboard', user)
+                .then(response => {
+                    console.log('POST request successful:', response.data);
+                })
+                .catch(error => {
+                    console.error('Error making POST request:', error);
+                });
+        }
+    });
+
+    // Function to delete user by _id
+    window.deleteUser = function (userId) {
+        // Make a DELETE request to crudcrud.com using Axios
+        axios.delete(`https://crudcrud.com/api/59fd66bbab264efbbbcd1508a0684487/dashboard/${userId}`)
+            .then(response => {
+                console.log('DELETE request successful:', response.data);
+
+                // Remove the deleted user from the local data
+                existingUserData = existingUserData.filter(user => user._id !== userId);
+
+                // Update the local storage with the modified user data
+                localStorage.setItem("userData", JSON.stringify(existingUserData));
+
+                // Display the updated user data
+                displayUserData();
+            })
+            .catch(error => {
+                console.error('Error making DELETE request:', error);
+            });
+    };
+});

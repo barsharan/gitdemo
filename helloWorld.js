7b534189f35c224a5cbb9b4c3020bfb9d029df91
barsharan <barsharani597@gmail.com>
2024-02-15 19:44:08 +0530

    add hello world file

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+// Define a function to print "Hello, World!" to the console
+function printHelloWorld() {
+  console.log("Hello, World!");
+}
+
+// Call the function to print "Hello, World!"
+printHelloWorld();
